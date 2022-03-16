845fd741bc54a2c7880ddf8cb5234f5cebd7acf2
Artem Malyi <artem.malyi@gmail.com>
2022-03-16 16:37:21 +0200

    made precompiled headers shared and added logger

--- src/com-object-server/dllmain.cpp
+++ src/com-object-server/dllmain.cpp
@@ -1,11 +1,17 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
-#include "pch.h"
+#include "..\common\pch.h"
+
+#define DEBUG_LOGGER_ENABLED
+#define FILE_LOGGER_ENABLED
+#define LOG_PREFIX "[COM-OBJ]"
+#include "logger.h"
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
                      )
 {
+    LOG("Entering, hModule: 0x%p, ul_reason_for_call: 0x%08x, lpReseved: 0x%p");
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
