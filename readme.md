aad702cbbf4398f7a0133fc2c65ad7310504f2bc
Artem Malyi <artem.malyi@gmail.com>
2022-03-16 15:39:42 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+# COM object template
---
+++ src/com-object-server/AddObj.h
@@ -0,0 +1,10 @@
+#pragma once
+
+///////////////////////////////////////////////////////////
+//
+// AddObj.h
+// Contains the C++ class declarations for implementing the IAdd interface
+//
+
+#include "IAdd_h.h"
+
